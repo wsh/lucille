f9c251b78147f8859ec2e9d9d0f0104919d50912
Will Hayworth <whayworth@gmail.com>
2013-02-24 17:31:40 -0500

    Switching back and forth properly.

--- app.js
+++ app.js
@@ -7,22 +7,34 @@ var lucilleOptions = {
     passThrough: true
 };
 
+http.createServer(function (req, res){
+    res.end("yay!");
+}).listen(3001);
+
+var lucilleProxy = new prx.HttpProxy({
+  target: {
+    host: 'localhost', 
+    port: 3001
+  }
+});
+
 http.createServer(function (req, res) {
   if(req.headers["x-lucille-command"] !== undefined){
       processOptions(lucilleOptions, req, res);
   }else{
-      processRequest(lucilleOptions, req, res);
+      processRequest(lucilleOptions, req, res, lucilleProxy);
   }
-}).listen(process.env.PORT);
+}).listen(3000);
 
 var processOptions = function(options, req, res){
-    console.log("parsing options");
+    console.log("processing options");
     var body = '';
     req.on('data', function(data){ body += data; });
     req.on('end', function(){parseOptions(options, req, res, qs.parse(body))});
 };
 
 var parseOptions = function(options, req, res, body){
+    console.log("parsing options");
     var updatedOptions = updateOptions(options, body); 
     res.writeHead(200, {'Content-Type': 'text/json'});
     res.end(JSON.stringify(updatedOptions)+"\n");
@@ -30,26 +42,29 @@ var parseOptions = function(options, req, res, body){
 };
 
 var updateOptions = function(options, body){
+    console.log("updating options");
     _.each(options, function(v, k, orig){
         if(body[k] !== undefined){
-            orig[k] = body[k];
+            orig[k] = JSON.parse(body[k]);
         }
     });
     return options;
 };
 
-var processRequest = function(options, req, res){
+var processRequest = function(options, req, res, proxy){
+    console.log("processing request");
     switch(options.passThrough){
-        case true: passThroughReq(req, res); break;
-        case false: processReqWithOptions(options, req, res); break;
+        case true: passThroughReq(options, req, res, proxy); break;
+        case false: processReqWithOptions(options, req, res, proxy); break;
     }
 };
 
-var passThroughReq = function(req, res){
-    res.end("should've been a passthrough");
-    // passthru function from proxy lib goes here
+var passThroughReq = function(options, req, res, proxy){
+    console.log("passing through");
+    proxy.proxyRequest(req, res);
 };
 
-var processReqWithOptions = function(options, req, res){
-    
+var processReqWithOptions = function(options, req, res, proxy){
+    res.write("faked something");
+    proxy.proxyRequest(req, res);
 };
